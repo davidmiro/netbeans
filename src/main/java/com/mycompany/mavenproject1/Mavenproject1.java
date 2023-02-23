d6a6a950d72c96199056ae80ea4ca6310bff3064
david <david@vostro-14>
2023-02-22 21:53:36 -0500

    First commit

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author david
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+    }
+}
